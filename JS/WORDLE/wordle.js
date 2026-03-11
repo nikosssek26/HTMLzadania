b93714106dcf36c4f0479ac45e28c958d8a5c374
nivks <uT_4236@zsp3zamosc.pl>
2026-03-11 13:57:01 +0100

    śr. 11.03.2026

--- JS/WORDLE/wordle.js
+++ JS/WORDLE/wordle.js
@@ -1,4 +1,6 @@
-let zagadka = "DOMEK";
+let zagadka = "ALLAH";
+let nrWiersza = 0;
+let proby = 5;
 
 function StartGame(){
     console.log("Game started");
@@ -7,6 +9,40 @@ function StartGame(){
 function Sprawdz(){
     let input = document.getElementById("input").value.toUpperCase();
     console.log(input);
+    document.getElementById("input").value = "";
+    document.getElementById("buttonsprawdz").disabled = true;
+
+    let tbl = document.getElementById("thisTable");
+
+    for(let i = 0; i < input.length; i++){
+        tbl.rows[nrWiersza].cells.item(i).setAttribute("id", "cell"+nrWiersza+i);
+        document.getElementById("cell"+nrWiersza+i).innerHTML = input[i];
+    }
+    let tempzagadka = zagadka;
+    for ( let i =0; i<input.length; i++){
+        if(tempzagadka[i]==input[i]){
+            document.getElementById('cell'+nrWiersza+i).style.backgroundColor= 'green';
+            tempzagadka[i] = "*"
+        }
+        else{
+            tempzagadka+= zagadka[i]
+        }
+    }
+    let t = tempzagadka;
+    tempzagadka = '';
+    for( let i =0; i<input.length; i++){
+        if(input[i] == tempzagadka[i]){
+            document.getElementById('cell'+nrWiersza+i).style.backgroundColor= 'yellow';
+            break;
+        }
+    }
+
+    nrWiersza++;
+    proby--;
+    if(proby == 0){
+        alert("Koniec gry! Prawidłowe słowo to: " + zagadka);
+        location.reload();
+    }
     
 }
 
@@ -14,6 +50,7 @@ function SprawdzEnable(){
     let input = document.getElementById("input").value.toUpperCase();
     console.log(input);
     document.getElementById("buttonsprawdz").disabled = (input.length != 5);
+
 }
 
 function Tabela(){
@@ -21,6 +58,4 @@ function Tabela(){
     let i = 0;
     let j = 0;
     tbl.rows[i].cells.item(j).setAttribute("id", "cell"+i+j);
-    document.getElementById("cell"+i+j).style.backgroundColor = "green";
-    document.getElementById("cell"+i+j).innerHTML = "A";
 }
